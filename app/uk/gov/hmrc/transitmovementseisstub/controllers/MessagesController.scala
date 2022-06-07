5bdca38661bc114e384ba2b250ebb8267b188f19
liandra-giacomine <liandra.giacomine@opencastsoftware.com>
2022-06-07 12:09:22 +0100

    New endpoint to accept messages

--- app/uk/gov/hmrc/transitmovementseisstub/controllers/MessagesController.scala
+++ app/uk/gov/hmrc/transitmovementseisstub/controllers/MessagesController.scala
@@ -16,18 +16,18 @@
 
 package uk.gov.hmrc.transitmovementseisstub.controllers
 
-import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import play.api.mvc.Action
 import play.api.mvc.AnyContent
 import play.api.mvc.ControllerComponents
+import play.api.mvc.Request
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
 import javax.inject.Inject
-import javax.inject.Singleton
-import scala.concurrent.Future
 
-@Singleton()
-class MicroserviceHelloWorldController @Inject() (cc: ControllerComponents) extends BackendController(cc) {
+class MessagesController @Inject() (cc: ControllerComponents) extends BackendController(cc) {
 
-  def hello(): Action[AnyContent] = Action.async {
-    Future.successful(Ok("Hello world"))
+  def post(): Action[AnyContent] = Action {
+    _: Request[AnyContent] =>
+      Accepted
   }
 }
