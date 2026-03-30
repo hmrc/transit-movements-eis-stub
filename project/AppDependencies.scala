c51e2a0aef32bab1e1a7c1a216cfc49cc812142e
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2026-03-30 13:54:18 +0100

    CTCP6-229: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,27 +3,29 @@ import sbt._
 
 object AppDependencies {
 
-  private val catsVersion = "2.7.0"
-  private val catsRetryVersion = "3.1.3"
+  private val catsVersion = "2.13.0"
+  private val bootstrapVersion = "10.7.0"
+  private val catsRetryVersion = "4.0.0"
+  private val pekkoVersion = "1.4.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % "9.11.0",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
     "org.typelevel" %% "cats-core" % catsVersion,
     "com.github.cb372" %% "cats-retry" % catsRetryVersion,
-    "org.apache.pekko" %% "pekko-slf4j" % "1.1.3",
-    "org.apache.pekko" %% "pekko-connectors-xml" % "1.1.0",
-    "org.apache.pekko" %% "pekko-protobuf-v3" % "1.1.3",
-    "org.apache.pekko" %% "pekko-serialization-jackson" % "1.1.3",
-    "org.apache.pekko" %% "pekko-stream" % "1.1.3",
-    "org.apache.pekko" %% "pekko-actor-typed" % "1.1.3",
+    "org.apache.pekko" %% "pekko-slf4j" % pekkoVersion,
+    "org.apache.pekko" %% "pekko-connectors-xml" % "1.3.0",
+    "org.apache.pekko" %% "pekko-protobuf-v3" % pekkoVersion,
+    "org.apache.pekko" %% "pekko-serialization-jackson" % pekkoVersion,
+    "org.apache.pekko" %% "pekko-stream" % pekkoVersion,
+    "org.apache.pekko" %% "pekko-actor-typed" % pekkoVersion,
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % "9.11.0",
+    "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % bootstrapVersion,
     "org.scalatestplus" %% "mockito-5-12"            % "3.2.19.0",
-    "org.scalacheck"    %% "scalacheck"              % "1.18.1",
+    "org.scalacheck"    %% "scalacheck"              % "1.19.0",
     "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0",
-    "org.apache.pekko" %% "pekko-serialization-jackson" % "1.1.3",
-    "org.apache.pekko" %% "pekko-actor-typed" % "1.1.3"
+    "org.apache.pekko" %% "pekko-serialization-jackson" % pekkoVersion,
+    "org.apache.pekko" %% "pekko-actor-typed" % pekkoVersion
   ).map(_ % Test)
 }
